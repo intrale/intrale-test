61686422811aee9b6359ba79035112e0da0c52fd
llarreta <leonel@larreta.com.ar>
2021-02-04 16:34:36 -0300

    Mejoras

--- src/main/java/ar/com/intrale/cloud/Test.java
+++ src/main/java/ar/com/intrale/cloud/Test.java
@@ -15,69 +15,69 @@ import io.micronaut.context.ApplicationContext;
 import io.micronaut.core.beans.BeanIntrospection;
 import io.micronaut.runtime.EmbeddedApplication;
 
-public abstract class Test <PROV>{
+public abstract class Test<PROV> {
 
-	   public static final String DUMMY_VALUE = "DUMMY";
-	   public static final String DUMMY_EMAIL = "DUMMY@DUMMY.COM";
-	   public static final String DUMMY_PASS = "123#abCD";
-	   public static final String DUMMY_PASS_2 = "456#efGH";
-	   
-	   private final Class<Request> providerType = (Class<Request>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-	
-	   @Inject
-	   protected ApplicationContext applicationContext;
-	   
-	   @Inject
-	   protected EmbeddedApplication app;
-	   
-	   protected ObjectMapper mapper = new ObjectMapper();
-	   
-	   protected static Lambda lambda;
-	   
-	   protected PROV provider;
-	   
-	   
-	   /**
-	    * Instancia Lambda y setea el proveedor en caso de que corresponda
-	    */
-	   public void lambdaInstantiation() {
-		   if (lambda==null) {
-			   BeanIntrospection<Lambda> beanIntrospection = BeanIntrospection.getIntrospection(Lambda.class);
-			   lambda = beanIntrospection.instantiate();
-		   }
-		   if (provider!=null) {
-			   lambda.getFunction().setProvider(provider);
-		   }
-	   }
-	   
-	   /**
-	    * Se ejecuta por unica vez previo a todos los test
-	    */
-	   @BeforeAll
-	   public static void setup() {
-	   }
-	   
-	   @BeforeEach
-	   public void initializeTest() {
-		   beforeEach();
-		   lambdaInstantiation();
-	   }
-	   
-	   public abstract void beforeEach();
-	   
-	   @AfterEach
-	   public void finalizeTest() {
-		   afterEach();
-	   }
-	   
-	   public abstract void afterEach();
-	   
-	   @AfterAll
-	   public static void end() {
-			if (lambda != null) {
-	            lambda.getApplicationContext().close();
-	        }
-	   }
-	   
-	   
+	public static final Long   DUMMY_ID = Long.valueOf(777);
+	public static final String DUMMY_VALUE = "DUMMY";
+	public static final String DUMMY_EMAIL = "DUMMY@DUMMY.COM";
+	public static final String DUMMY_PASS = "123#abCD";
+	public static final String DUMMY_PASS_2 = "456#efGH";
+
+	private final Class<Request> providerType = (Class<Request>) ((ParameterizedType) getClass().getGenericSuperclass())
+			.getActualTypeArguments()[0];
+
+	@Inject
+	protected ApplicationContext applicationContext;
+
+	@Inject
+	protected EmbeddedApplication app;
+
+	protected ObjectMapper mapper = new ObjectMapper();
+
+	protected static Lambda lambda;
+
+	protected PROV provider;
+
+	/**
+	 * Instancia Lambda y setea el proveedor en caso de que corresponda
+	 */
+	public void lambdaInstantiation() {
+		if (lambda == null) {
+			BeanIntrospection<Lambda> beanIntrospection = BeanIntrospection.getIntrospection(Lambda.class);
+			lambda = beanIntrospection.instantiate();
+		}
+		if (provider != null) {
+			lambda.getFunction().setProvider(provider);
+		}
+	}
+
+	/**
+	 * Se ejecuta por unica vez previo a todos los test
+	 */
+	@BeforeAll
+	public static void setup() {
+	}
+
+	@BeforeEach
+	public void initializeTest() {
+		beforeEach();
+		lambdaInstantiation();
+	}
+
+	public abstract void beforeEach();
+
+	@AfterEach
+	public void finalizeTest() {
+		afterEach();
+	}
+
+	public abstract void afterEach();
+
+	@AfterAll
+	public static void end() {
+		if (lambda != null) {
+			lambda.getApplicationContext().close();
+		}
+	}
+
 }
